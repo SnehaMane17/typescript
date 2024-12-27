ef1287aa94bfd720e6803d99932a65e0c93f00bf
Sneha Marathe <sneha@Siddharths-MacBook-Pro.local>
2024-12-27 16:15:31 +0530

    first init code of typescript

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+function addNumbers(a, b) {
+    const result = a + b;
+    console.log(result);
+}
+addNumbers(2, 2);
+  
---
+++ index.ts
@@ -0,0 +1,6 @@
+
+function addNumbers(a:number, b:number){
+    const result = a + b
+    console.log(result)
+}
+addNumbers(2, 2)
